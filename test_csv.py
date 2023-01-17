75d6df6b0373d516e051b89bc3a555c16a5fb4d8
SantiLP <santiluciaprivi@gmail.com>
2023-01-17 09:22:14 -0300

    cambios

--- test_csv.py
+++ test_csv.py
@@ -1,10 +1,9 @@
 import csv
 from pathlib import Path
 
-FILE_PATH = Path('data.csv')
+FILE_PATH = Path('a_procesar.csv')
 
 with FILE_PATH.open(mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     for row in reader:
         print(row['numero_expediente'])
-        
